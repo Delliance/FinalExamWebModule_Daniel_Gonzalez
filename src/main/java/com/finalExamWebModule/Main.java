5b8735c673859d428539eb6862e5a154e9e40405
danielgn95 <daniel.gn@globant.com>
2022-04-22 12:01:51 -0500

    Initial Commit

--- src/main/java/com/finalExamWebModule/Main.java
+++ src/main/java/com/finalExamWebModule/Main.java
@@ -1,4 +1,6 @@
-package com.finalExamWebModule;public class Main {
+package com.finalExamWebModule;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
